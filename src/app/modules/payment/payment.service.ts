50377d321c70c298ad1c11e41c845b9539a4d00a
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-11-11 20:18:35 +0600

    payment methond initaited

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -1,17 +1,27 @@
+import { readFileSync } from "fs";
 import { paymentStatus } from "../booking/booking.constant";
 import { Booking } from "../booking/booking.model";
 import { verifyPayment } from "./payment.utils";
+import { join } from "path";
 
 const confirmationService = async (transactionId: string, status: string) => {
   const verifyResponse = await verifyPayment(transactionId);
   let result;
+  let message = "";
   if (verifyResponse.pay_status === "Successful") {
     result = await Booking.findOneAndUpdate(
       { transactionId },
       { paymentStatus: paymentStatus.success, isBooked: "confirmed" }
     );
+    message = "Successfully paid";
+  } else {
+    message = "payment failed";
   }
-  return `<h1> Payment ${status}</h1>`;
+  const filePath = join(__dirname, "../../../view/confirmation.html");
+  let template = readFileSync(filePath, "utf-8");
+  // console.log({ template });
+  template = template.replace("{{message}}", message);
+  return template;
 };
 export const paymentService = {
   confirmationService,
--- src/app/modules/payment/payment.utils.ts
+++ src/app/modules/payment/payment.utils.ts
@@ -1,32 +1,37 @@
 import axios from "axios";
 import config from "../../config";
+import AppError from "../../errors/appError";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const initiatePayment = async (paymentData: any) => {
-  // console.log("PaymentData", paymentData);
-  const response = await axios.post(config.payment_url!, {
-    store_id: config.store_id,
-    signature_key: config.signature_key,
-    tran_id: paymentData.transactionId,
-    success_url: `http://localhost:5000/api/payment/confirmation?transactionId=${paymentData.transactionId}&status=success`,
-    fail_url: `http://localhost:5000/api/payment/confirmation?&status=failed`,
-    cancel_url: "http://localhost:5173/",
-    amount: paymentData.totalPrice,
-    currency: "BDT",
-    desc: "Merchant Registration Payment",
-    cus_name: paymentData.customerName,
-    cus_email: paymentData.customerEmail,
-    cus_add1: paymentData.customerAddress,
-    cus_add2: "N/A",
-    cus_city: "N/A",
-    cus_state: "N/A",
-    cus_postcode: "N/A",
-    cus_country: "Bangladesh",
-    cus_phone: paymentData.customerPhone,
-    type: "json",
-  });
-  // console.log(response);
-  return response.data;
+  try {
+    // console.log("PaymentData", paymentData);
+    const response = await axios.post(config.payment_url!, {
+      store_id: config.store_id,
+      signature_key: config.signature_key,
+      tran_id: paymentData.transactionId,
+      success_url: `http://localhost:5000/api/payment/confirmation?transactionId=${paymentData.transactionId}&status=success`,
+      fail_url: `http://localhost:5000/api/payment/confirmation?&status=failed`,
+      cancel_url: "http://localhost:5173/",
+      amount: paymentData.totalPrice,
+      currency: "BDT",
+      desc: "Merchant Registration Payment",
+      cus_name: paymentData.customerName,
+      cus_email: paymentData.customerEmail,
+      cus_add1: paymentData.customerAddress,
+      cus_add2: "N/A",
+      cus_city: "N/A",
+      cus_state: "N/A",
+      cus_postcode: "N/A",
+      cus_country: "Bangladesh",
+      cus_phone: paymentData.customerPhone,
+      type: "json",
+    });
+    // console.log(response);
+    return response.data;
+  } catch (error) {
+    throw new Error("there is some issue  in payment url ");
+  }
 };
 
 export const verifyPayment = async (tnxId: string) => {
