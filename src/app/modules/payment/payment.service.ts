c6acb2de125a021c9df48bc3ad62d29ef8dbc208
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-11-13 11:49:17 +0600

    transiction id related issue resolved

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -9,10 +9,25 @@ const confirmationService = async (transactionId: string, status: string) => {
   let result;
   let message = "";
   if (verifyResponse.pay_status === "Successful") {
-    result = await Booking.findOneAndUpdate(
-      { transactionId },
-      { paymentStatus: paymentStatus.success, isBooked: "confirmed" }
-    );
+    try {
+      result = await Booking.findOneAndUpdate(
+        { transactionId },
+        { paymentStatus: paymentStatus.success, isBooked: "confirmed" },
+        { new: true } // This returns the updated document
+      );
+    } catch (error) {
+      console.error("Error updating booking:", error);
+    }
+
+    // console.log(
+    //   "pay status status",
+    //   verifyResponse.pay_status,
+    //   "tranID",
+    //   transactionId,
+    //   "status: ",
+    //   status
+    // );
+    // console.log(result);
     message = "Successfully paid";
   } else {
     message = "payment failed";
